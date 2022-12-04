28672aa730795d9c8d839a89ed1e77ff1f3bde13
Iva Petkova <i.petkova@dynamosoftware.com>
2022-12-04 12:30:41 +0200

    amend day 4 solution

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -34,16 +34,16 @@ private fun findPairsFullyContainingTheOther() {
         if (it.first.containsAll(it.second) || it.second.containsAll(it.first)) totalOverlappingPairs++
     }
 }
-private fun getElfRange(range: List<String>): List<Int> {
-    val list = mutableListOf<Int>()
+private fun getElfRange(range: List<String>): Set<Int> {
+    val list = mutableSetOf<Int>()
     for (i in range[0].toInt() until range[1].toInt() + 1) { list.add(i) }
     return list
 }
 
-private fun readInput(): List<Pair<List<Int>, List<Int>>> {
+private fun readInput(): List<Pair<Set<Int>, Set<Int>>> {
     return File(PATH).readLines().map { it.split(",").toList() }.map {
-        val first = getElfRange(it[0].split("-").toList())
-        val second = getElfRange(it[1].split("-").toList())
+        val first = getElfRange(it[0].split("-")).toSet()
+        val second = getElfRange(it[1].split("-")).toSet()
         Pair(first, second)
     }.toList()
 }
