57f69b169fb29ed03a0d6657ede7cff711444401
Iva Petkova <i.petkova@dynamosoftware.com>
2022-12-06 16:01:24 +0200

    refactor day 6

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -4,31 +4,20 @@ import java.io.File
 
 const val PATH = "src/main/kotlin/input/input6.txt"
 var part = 0
-val input = readInput()
 fun main() {
     println("Select part:")
     part = readln().toInt()
-    when (part) {
+    val answer = when (part) {
         1 -> findMarker(4)
         2 -> findMarker(14)
+        else -> -1
     }
+    println(answer)
 }
 
 //===========================================================================================================
-private fun findMarker(distinctCharacters: Int) {
-    val list = mutableListOf<Char>()
-    for (index in input.indices) {
-        list.add(input[index])
-        if (list.size >= distinctCharacters) {
-            val subset = list.subList(list.size - distinctCharacters, list.size).toSet()
-            if (subset.size == distinctCharacters) {
-               println(index + 1)
-                break
-            }
-        }
-    }
-}
 
-private fun readInput(): CharArray {
-    return File(PATH).readText().toCharArray()
-}
+private fun findMarker(distinctChars: Int) =
+    File(PATH).readText().toCharArray().toList().windowed(distinctChars).indexOfFirst {
+        it.toSet().size == it.size
+    } + distinctChars
