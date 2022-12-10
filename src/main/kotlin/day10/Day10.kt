600d5736ca44017fa4c8fbae2cc8587183f5fe91
Iva Petkova <i.petkova@dynamosoftware.com>
2022-12-10 13:38:09 +0200

    day 10 part 2 solution

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -1,6 +1,7 @@
 package day10
 
 import java.io.File
+import kotlin.text.StringBuilder
 
 const val PATH = "src/main/kotlin/input/input10.txt"
 var part = 0
@@ -9,25 +10,61 @@ val program = readInput()
 var cycle = 0
 var xValue = 1
 var results = getResultsMap()
-
+var output = StringBuilder()
 fun main() {
-
+    println("Select part:")
+    part = readln().toInt()
     program.forEach {
-        calculateX(it)
+        when (part) {
+            1 -> calculateX(it)
+            2 -> renderImage(it)
+        }
     }
-    println(calculateSignalStrength())
+    if (part == 1) println(calculateSignalStrength()) else output.toString().chunked(40).forEach { println(it) }
 }
+
+fun renderImage(line: List<String>) {
+    calculateXPartTwo(line)
+}
+
+private fun calculateXPartTwo(line: List<String>) {
+    when (line[0]) {
+        "noop" -> handleNoopInstructionPartTwo()
+        "addx" -> handleAddInstructionPartTwo(line[1].toInt())
+    }
+}
+
+private fun handleAddInstructionPartTwo(value: Int) {
+    repeat(2) {
+        cycle++
+        draw()
+        resetCycleIfNeeded()
+    }
+    xValue += value
+}
+
+fun resetCycleIfNeeded() {
+    if (cycle >= 40) cycle = 0
+}
+
+private fun handleNoopInstructionPartTwo() {
+    cycle++
+    draw()
+    resetCycleIfNeeded()
+}
+
+private fun draw() = if (cycle - 1 in xValue - 1..xValue + 1) output.append("#") else output.append(" ")
 
 fun calculateSignalStrength() = results.map { it.key * it.value }.sum()
 
-private fun calculateX(it: List<String>) {
-    when (it[0]) {
+private fun calculateX(line: List<String>) {
+    when (line[0]) {
         "noop" -> handleNoopInstruction()
-        "addx" -> handleAddInstruction(it[1].toInt())
+        "addx" -> handleAddInstructionPartOne(line[1].toInt())
     }
 }
 
-private fun handleAddInstruction(value: Int) {
+private fun handleAddInstructionPartOne(value: Int) {
     cycle++
     addXValueIfNeeded()
     cycle++
@@ -57,6 +94,5 @@ fun getResultsMap(): MutableMap<Int, Int> {
 }
 
 //================================================================================================================
-
 
 private fun readInput() = File(PATH).readLines().map { it.split("\\s+".toRegex()) }
