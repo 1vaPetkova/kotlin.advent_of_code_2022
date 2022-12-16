0e30f94669615d3e7007a5988b2aa23c333594f9
Iva Petkova <i.petkova@dynamosoftware.com>
2022-12-16 12:55:16 +0200

    day 12 part 2

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -7,12 +7,51 @@ const val PATH = "src/main/kotlin/input/input12.txt"
 var part = 0
 val elements = readInput()
 
-val start = setStartAndDestinationValues('S', 'a')
+var start = setStartAndDestinationValues('S', 'a')
 val destination = setStartAndDestinationValues('E', 'z')
 var queue = ArrayDeque<Element>()
 
+
 fun main() {
-    val tm = System.currentTimeMillis()
+    val aElements = findAllAElements().map { Element(it.row, it.col, it.value, false, 0) }
+    println("Select part:")
+    part = readln().toInt()
+    val copy = elements.toList()
+
+    when (part) {
+        1 -> {
+            println(findShortestPathFromStartPosition())
+        }
+
+        2 -> {
+            val tm = System.currentTimeMillis()
+            val paths = mutableListOf<Int>()
+            aElements.forEach { e ->
+                start = e
+                val shortestPath = findShortestPathFromStartPosition()
+                if (shortestPath != 0) paths.add(shortestPath)
+                resetElements()
+                queue.clear()
+            }
+
+            println(System.currentTimeMillis() - tm)
+            println(paths.min())
+        }
+    }
+}
+
+private fun resetElements() {
+    elements.forEach {
+        it.forEach { e ->
+            e.visited = false
+            e.stepsToReach = 0
+        }
+    }
+}
+
+//================================================================================================================
+
+private fun findShortestPathFromStartPosition(): Int {
     start.visited = true
     queue.offer(start)
 
@@ -21,27 +60,27 @@ fun main() {
         if (element == destination) break
         addAdjacentElements(element.row, element.col)
     }
-    println(System.currentTimeMillis() - tm)
-    println(elements.getElement(destination.row, destination.col).stepsToReach)
+    return elements.getElement(destination.row, destination.col).stepsToReach
 }
 
-//================================================================================================================
+fun findAllAElements() = elements.flatten().filter { it.value == 'a' }
 
-fun addAdjacentElements(row: Int, col: Int) {
+
+private fun addAdjacentElements(row: Int, col: Int) {
     if (shouldVisit(row, 1, col, 0)) addAdjacentElement(row, 1, col, 0)
     if (shouldVisit(row, 0, col, 1)) addAdjacentElement(row, 0, col, 1)
     if (shouldVisit(row, -1, col, 0)) addAdjacentElement(row, -1, col, 0)
     if (shouldVisit(row, 0, col, -1)) addAdjacentElement(row, 0, col, -1)
 }
 
-fun addAdjacentElement(row: Int, deltaRow: Int, col: Int, deltaCol: Int) {
+private fun addAdjacentElement(row: Int, deltaRow: Int, col: Int, deltaCol: Int) {
     val nextElement = elements.getElement(row + deltaRow, col + deltaCol)
     nextElement.stepsToReach = elements.getElement(row, col).stepsToReach + 1
     nextElement.visited = true
     queue.offer(nextElement)
 }
 
-fun shouldVisit(row: Int, deltaRow: Int, col: Int, deltaCol: Int) =
+private fun shouldVisit(row: Int, deltaRow: Int, col: Int, deltaCol: Int) =
     isInBounds(row + deltaRow, col + deltaCol)
             && isAccessible(row, deltaRow, col, deltaCol)
             && !elements.getElement(row + deltaRow, col + deltaCol).visited
